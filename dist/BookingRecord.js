999a7b238ce57de6e0bd4472164d8d50fcf26e1a
fairychan <butsaranee.no60@cbs.chula.ac.th>
2020-07-02 16:25:36 +0700

    deleted dist

--- dist/BookingRecord.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class BookingRecord {
-    constructor(roomNumber, guestData) {
-        this.roomNumber = roomNumber;
-        this.guestData = guestData;
-    }
-}
--- dist/Keycard.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Keycard = void 0;
-class Keycard {
-    constructor(id) {
-        this.id = id;
-    }
-}
-exports.Keycard = Keycard;
